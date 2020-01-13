8e84a3130db3029fe68625b874f9bb8b81549e47
dork7 <weeday425@gmail.com>
2020-01-13 17:32:36 +0500

    13-01-2020 bak

--- content.js
+++ content.js
@@ -1,16 +1,16 @@
-
-function ping() {
-  chrome.runtime.sendMessage('ping', response => {
-    if(chrome.runtime.lastError) {
-      setTimeout(ping, 1000);
-    } else {
-      // alert("ready");
-      // Do whatever you want, background script is ready now
-      console.log(response);
+///// just some random code xX
+// function ping() {
+//   chrome.runtime.sendMessage('ping', response => {
+//     if(chrome.runtime.lastError) {
+//       setTimeout(ping, 1000);
+//     } else {
+//       // alert("ready");
+//       // Do whatever you want, background script is ready now
+//       console.log(response);
             
-    }
-  });
-}
-document.addEventListener('click', () => alert('Click occurred! in content'));
+//     }
+//   });
+// }
+// //document.addEventListener('click', () => alert('Click occurred! in content'));
 
-ping();
+// ping();
--- popup.js
+++ popup.js
@@ -9,9 +9,16 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //chrome.runtime.onMessage.addListener((request, sender, sendResponse) => sendResponse('pong'));
 
 
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-    width : 100,
-    height : 100
-});
 
-qrcode.makeCode("https://www.youtube.com/user/VoidRealms");
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    makeCode(url);
+    
+});
+function makeCode(url){
+    var qrcode = new QRCode(document.getElementById("qrcode"), {
+        width : 200,
+        height : 200
+    });
+    qrcode.makeCode(url);
+}

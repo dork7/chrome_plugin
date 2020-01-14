a4a994395622c35b12b0412df7192dd2414bcf82
dork7 <weeday425@gmail.com>
2020-01-14 17:55:19 +0500

    youtube time thing

--- content.js
+++ content.js
@@ -1,16 +1,19 @@
 ///// just some random code xX
-// function ping() {
-//   chrome.runtime.sendMessage('ping', response => {
-//     if(chrome.runtime.lastError) {
-//       setTimeout(ping, 1000);
-//     } else {
-//       // alert("ready");
-//       // Do whatever you want, background script is ready now
-//       console.log(response);
+function ping() {
+  chrome.runtime.sendMessage('ping', response => {
+    if(chrome.runtime.lastError) {
+      setTimeout(ping, 1000);
+    } else {
+        var video = document.getElementsByTagName("video")[0];
+        alert(video);
+      // alert("ready");
+      // Do whatever you want, background script is ready now
+      console.log(video.currentTime);
             
-//     }
-//   });
-// }
+    }
+  });
+}
 // //document.addEventListener('click', () => alert('Click occurred! in content'));
 
-// ping();
+ping();
+console.log("content script");
--- popup.js
+++ popup.js
@@ -3,22 +3,30 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
   sendResponse({url: window.location.href})
 
-})
+}) // sending response to content.js
 
 
-//chrome.runtime.onMessage.addListener((request, sender, sendResponse) => sendResponse('pong'));
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => sendResponse('pong')); //sending response to content.js
 
 
 
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
+
+
     makeCode(url);
     
+    console.log(url);
 });
-function makeCode(url){
+function makeCode(url){ 
     var qrcode = new QRCode(document.getElementById("qrcode"), {
         width : 200,
         height : 200
     });
     qrcode.makeCode(url);
 }
+
+ 
+ // var vid = $('video').get(0);
+ // alert(vid);
+ // console.log(vid.getCurrentTime());

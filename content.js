3d334a2237ed8c49623c666c1c3ec48f37dd212f
dork7 <weeday425@gmail.com>
2020-01-16 12:10:07 +0500

    16-01-2020 bak

--- content.js
+++ content.js
@@ -1,12 +1,13 @@
 // this portion is used for youtube .it send play time of youtube video back to popup 
 chrome.runtime.onConnect.addListener((port) => {
   port.onMessage.addListener((msg) => {
+    // console.log(msg);
     if (msg.function == 'html') {
       var video = document.getElementsByTagName("video")[0];
-      console.log(video.currentTime);
+      //console.log(video.currentTime);
       port.postMessage({ vtime: video.currentTime , title: document.title });
-    }
+      }
   });
 });
 
-console.log("content script");
+ 
--- popup.js
+++ popup.js
@@ -5,19 +5,21 @@ window.addEventListener('load', (event) => {
         );
 });
 
-function connect() {
-   document.getElementById("qrcode").innerhtml = "";
 
-   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const port = chrome.tabs.connect(tabs[0].id);
+function connect() {
+ document.getElementById("qrcode").innerhtml = "";
+
+ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     var url = tabs[0].url;
     if (url.includes("watch"))
     {
+        var port = chrome.tabs.connect(tabs[0].id);
+        document.getElementById("qrcode").innerhtml = "";
         port.postMessage({ function: 'html' });
         port.onMessage.addListener((response) => {
           vtime = response.vtime;
           title = response.title;
-
+          response = "";
 
         mURL = url.concat("?t=",vtime); // getting url and concatinatin with time
 
@@ -27,12 +29,16 @@ function connect() {
 //        const div = document.createElement('div')
 //        div.textContent = mURL
 //      document.body.appendChild(div)
-});
+
+
+
+         port.disconnect();
+    });
     }
     else {
         // if the current tab is not a youtube video this will execute
-       makeCode(url);
-   }
+        makeCode(url);
+    }
 
 
 });
